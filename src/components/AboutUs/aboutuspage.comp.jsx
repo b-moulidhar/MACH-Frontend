fe93335d514cc0feb2575bb75080a9baca4b904f
Moulidhar B <65845778+b-moulidhar@users.noreply.github.com>
2024-07-02 15:49:34 +0530

    added styling to about us (#7)
    Co-authored-by: Moulidhar B <moulidhar.b@valtech.com>

--- src/components/AboutUs/aboutuspage.comp.jsx
+++ src/components/AboutUs/aboutuspage.comp.jsx
@@ -1,22 +1,25 @@
 import Footer from "../Footer/footer.comp";
 import HeaderComp from "../Header/header.comp";
 import React, { useEffect, useState } from 'react';
+import "./aboutUs.css"
+import { Contentful } from "../../api/api";
 
 
 
 const AboutUsPage = () => {
-    const [data, setData] = useState(null);
+    const [aboutData, setAboutData] = useState(null);
     const [loading, setLoading] = useState(true);
   
     useEffect(() => {
       const fetchData = async () => {
         try {
-          const response = await fetch('https://cdn.contentful.com/spaces/gqzgjhtdpuk2/environments/master/entries/6ZeCVCRIkWpsJu5YzevJQ1?access_token=xMPuiA4hCCzRxXiMBU9TMMBasn2K2YMHxN6YrV9C1l4');
-          if (!response.ok) {
+          const response = await Contentful.get('6ZeCVCRIkWpsJu5YzevJQ1');
+          if (response.status==200) {
+            setAboutData(response.data);
+          }else{
             throw new Error('Network response was not ok');
           }
-          const jsonData = await response.json();
-          setData(jsonData);
+          
         } catch (error) {
           console.error('Error fetching data:', error);
         } finally {
@@ -37,9 +40,9 @@ const AboutUsPage = () => {
         ) : (
           <div className='about-container'>
             
-            <h1>{data.fields.title}</h1>
-            <p>{data.fields.shortDescription}</p>
-            <img src={data.fields.jsonImage[0].secure_url}></img>
+            <h1>{aboutData.fields.title}</h1>
+            <p>{aboutData.fields.shortDescription}</p>
+            <img src={aboutData.fields.jsonImage[0].secure_url}></img>
            
           </div>
         )}
